0daca5aaa04563a2a0fd5a31efd7157f677ebb7c
Saverio Proto <saverioproto@microsoft.com>
2022-10-14 11:53:25 +0200

    Remove unused variable

--- istio-on-aks/variables.tf
+++ istio-on-aks/variables.tf
@@ -9,12 +9,6 @@ variable "agents_size" {
   type        = string
 }
 
-variable "rbac_aad_admin_group_object_ids" {
-  description = "Object ID of Active Directory groups with admin access."
-  type        = list(string)
-  default     = null
-}
-
 variable "kubernetes_version" {
   description = "Specify which Kubernetes release to use. The default used is the latest Kubernetes version available in the region"
   type        = string
