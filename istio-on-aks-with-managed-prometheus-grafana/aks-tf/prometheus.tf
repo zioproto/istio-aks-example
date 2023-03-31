16793ba1f1bd03fba05695e1fd5e7aede57ad42d
Saverio Proto <saverioproto@microsoft.com>
2023-03-31 15:35:50 +0200

    Fix Azure Role Assignments for Grafana

--- istio-on-aks-with-managed-prometheus-grafana/aks-tf/prometheus.tf
+++ istio-on-aks-with-managed-prometheus-grafana/aks-tf/prometheus.tf
@@ -77,8 +77,6 @@ resource "azapi_resource" "dataCollectionRuleAssociation" {
   })
 }
 
-
-
 resource "azurerm_dashboard_grafana" "this" {
   name                              = "istio-grafana"
   resource_group_name               = azurerm_resource_group.this.name
@@ -87,9 +85,36 @@ resource "azurerm_dashboard_grafana" "this" {
   deterministic_outbound_ip_enabled = true
   public_network_access_enabled     = true
 
-  azure_monitor_workspace_integrations  {
-      resource_id = azapi_resource.prometheus.id
-    }
+  azure_monitor_workspace_integrations {
+    resource_id = azapi_resource.prometheus.id
+  }
 
+  identity {
+    type = "SystemAssigned"
+  }
+}
 
+data "azurerm_subscription" "current" {}
+
+# Give Managed Grafana instances access to read monitoring data in current subscription.
+resource "azurerm_role_assignment" "monitoring_reader" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Monitoring Reader"
+  principal_id         = azurerm_dashboard_grafana.this.identity[0].principal_id
+}
+
+# https://learn.microsoft.com/en-us/azure/azure-monitor/essentials/prometheus-grafana
+resource "azurerm_role_assignment" "monitoring_data_reader" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Monitoring Data Reader"
+  principal_id         = azurerm_dashboard_grafana.this.identity[0].principal_id
+}
+
+data "azurerm_client_config" "current" {}
+
+# Give current client admin access to Managed Grafana instance.
+resource "azurerm_role_assignment" "grafana_admin" {
+  scope                = azurerm_dashboard_grafana.this.id
+  role_definition_name = "Grafana Admin"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
