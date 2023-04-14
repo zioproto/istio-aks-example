e7fd3bde173fd749908ec3e41518da3c733b0360
Saverio Proto <saverioproto@microsoft.com>
2023-04-14 10:01:24 +0200

    kubectl provider not used here

--- istio-on-aks-with-managed-prometheus-grafana/aks-tf/provider.tf
+++ istio-on-aks-with-managed-prometheus-grafana/aks-tf/provider.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.10"
     }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
     azapi = {
       source  = "Azure/azapi"
       version = ">= 1.4.0, < 2.0"
@@ -24,9 +20,5 @@ provider "azurerm" {
   }
 }
 
-provider "kubectl" {
-  config_path = "~/.kube/config"
-
-}
 
 provider "azapi" {}
